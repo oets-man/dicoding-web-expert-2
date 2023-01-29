fda70874c6103871a0859f4ee16ad095d6e8f9fb
Usman <oets.man@gmail.com>
2023-01-29 10:27:56 +0700

    add spinner

--- restaurant-app-1/src/scripts/main.js
+++ restaurant-app-1/src/scripts/main.js
@@ -1,4 +1,5 @@
 import '../components/list-card';
+import '../components/load-spinner';
 import data from './data-source';
 import { URL_PICTURE } from '../config/url';
 
@@ -8,13 +9,19 @@ const main = () => {
 		try {
 			const results = await data.getRestaurant();
 			const { restaurants } = results;
+
 			restaurants.forEach((restaurant, index) => {
+				restaurant.picture = `${URL_PICTURE.SMALL}/${restaurant.pictureId}`;
+				const spinner = document.createElement('load-spinner');
+				const card = document.createElement('list-card');
+				card.item = restaurant;
 				setTimeout(() => {
-					restaurant.picture = `${URL_PICTURE.SMALL}/${restaurant.pictureId}`;
-					const list = document.createElement('list-card');
-					list.item = restaurant;
-					container.appendChild(list);
-				}, 250 * index);
+					container.appendChild(spinner);
+					setTimeout(() => {
+						container.appendChild(card);
+						container.removeChild(spinner);
+					}, 500);
+				}, 500 * index);
 			});
 		} catch (error) {
 			alert(error);
