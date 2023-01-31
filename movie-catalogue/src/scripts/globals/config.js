18356ee3d5c75ace327d37f6af25f7869432b307
Usman <oets.man@gmail.com>
2023-01-31 12:58:09 +0700

    movie - reques api

---
+++ movie-catalogue/src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '2449fe38b922b8a9dd7efc38d21665d8',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
