2aff7c5eea6744425667e16b3c55e0b7999ab871
Usman <oets.man@gmail.com>
2023-02-01 15:40:09 +0700

    movie add like movie

--- movie-catalogue/src/scripts/views/pages/detail.js
+++ movie-catalogue/src/scripts/views/pages/detail.js
@@ -1,6 +1,7 @@
 import TheMovieDbSource from '../../data/themoviedb-source';
 import UrlParser from '../../routes/url-parser';
-import { createMovieDetailTemplate, createLikeButtonTemplate } from '../templates/template-creator';
+import LikeButtonInitiator from '../../utils/like-button-initiator';
+import { createMovieDetailTemplate } from '../templates/template-creator';
 
 const Detail = {
   async render() {
@@ -14,10 +15,19 @@ const Detail = {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
     const movie = await TheMovieDbSource.detailMovie(url.id);
     const movieContainer = document.querySelector('#movie');
-    const likeButtonContainer = document.querySelector('#likeButtonContainer');
 
     movieContainer.innerHTML = createMovieDetailTemplate(movie);
-    likeButtonContainer.innerHTML = createLikeButtonTemplate();
+
+    LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: movie.id,
+        title: movie.title,
+        overview: movie.overview,
+        backdrop_path: movie.backdrop_path,
+        vote_average: movie.vote_average,
+      },
+    });
   },
 };
 
