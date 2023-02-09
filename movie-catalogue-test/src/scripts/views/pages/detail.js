0644fd7503c7e8627eb19e4b35feeaa432d2d07b
Usman Husnan <oets.man@gmail.com>
2023-02-09 15:41:35 +0700

    movie test - like unlike

--- movie-catalogue-test/src/scripts/views/pages/detail.js
+++ movie-catalogue-test/src/scripts/views/pages/detail.js
@@ -1,7 +1,7 @@
 import UrlParser from '../../routes/url-parser';
 import TheMovieDbSource from '../../data/themoviedb-source';
 import { createMovieDetailTemplate } from '../templates/template-creator';
-import LikeButtonInitiator from '../../utils/like-button-initiator';
+import LikeButtonInitiator from '../../utils/like-button-presenter';
 
 const Detail = {
   async render() {
