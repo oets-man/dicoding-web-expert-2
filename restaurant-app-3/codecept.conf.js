7b2ce7abea97c3410dd9834b68b6909f500d9b75
Usman <oets.man@gmail.com>
2023-02-17 17:15:49 +0700

    res3 - test e2e

--- restaurant-app-3/codecept.conf.js
+++ restaurant-app-3/codecept.conf.js
@@ -8,17 +8,17 @@ setCommonPlugins();
 
 /** @type {CodeceptJS.MainConfig} */
 exports.config = {
-	tests: 'e2e/**/*.spec.js',
-	output: 'e2e/outputs',
-	helpers: {
-		Puppeteer: {
-			url: 'http://localhost:9000',
-			show: true,
-			windowSize: '1200x900',
-		},
-	},
-	include: {
-		I: './steps_file.js',
-	},
-	name: 'restaurant-app-3',
-};
+  tests: 'e2e/**/*.spec.js',
+  output: 'e2e/outputs',
+  helpers: {
+    Puppeteer: {
+      url: 'http://localhost:9000',
+      show: true,
+      windowSize: '1200x900'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  name: 'restaurant-app-3'
+}
